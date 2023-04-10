c91289059d185e7734f3c922cbf5fb9315dc6797
Bohdan <60944532+BohdanNikolaychuk@users.noreply.github.com>
2023-04-10 16:49:35 +0300

    Update axios.ts

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8080/'
+export const BASE_URL = 'https://store-app-list.onrender.com/'
 
 export const actionByID = (id: string | undefined) => BASE_URL + 'product/' + id
